7243db69719c87630e319e4fcfc4baa6feac646b
anuzsubedi <anuzsubedi@hotmail.com>
2025-01-10 00:05:11 -0500

    Add uuid parameter to handleAuth function for user registration

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,13 @@
 import { findUserByEmail, createUser } from "../models/userModel.js";
 
-export const handleAuth = ({ email, name, image }) => {
+export const handleAuth = ({ uuid, email, name, image }) => {
+
     const existingUser = findUserByEmail(email);
 
     if (existingUser) {
         return existingUser; // Log in the user
     }
 
-    const userId = createUser({ email, name, image });
-    return { id: userId, email, name, image }; // Sign up the user
+    const userId = createUser({ uuid, email, name, image });
+    return { id: userId, uuid, email, name, image }; // Sign up the user
 };
