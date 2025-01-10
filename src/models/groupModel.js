e4aeaea7d414e2a9402cd22ef7f430ba0b12b9d3
anuzsubedi <anuzsubedi@hotmail.com>
2025-01-10 00:42:47 -0500

    Implement soft delete for groups and add query to retrieve groups by member email

--- src/models/groupModel.js
+++ src/models/groupModel.js
@@ -8,16 +8,26 @@ export const createGroup = (name, description, callback) => {
 };
 
 export const getAllGroups = (callback) => {
-    const query = 'SELECT * FROM groups';
+    const query = 'SELECT * FROM groups WHERE is_deleted = 0';
     db.all(query, [], callback);
 };
 
 export const getGroupById = (id, callback) => {
-    const query = 'SELECT * FROM groups WHERE id = ?';
+    const query = 'SELECT * FROM groups WHERE id = ? AND is_deleted = 0';
     db.get(query, [id], callback);
 };
 
 export const deleteGroup = (id, callback) => {
-    const query = 'DELETE FROM groups WHERE id = ?';
+    const query = 'UPDATE groups SET is_deleted = 1, deleted_at = CURRENT_TIMESTAMP WHERE id = ?';
     db.run(query, [id], callback);
 };
+
+export const getGroupsByMember = (userEmail, callback) => {
+    const query = `
+    SELECT g.id, g.name, g.description, g.created_at
+    FROM groups g
+    JOIN group_members gm ON g.id = gm.group_id
+    WHERE gm.user_email = ? AND gm.is_deleted = 0 AND g.is_deleted = 0
+    `;
+    db.all(query, [userEmail], callback);
+}
