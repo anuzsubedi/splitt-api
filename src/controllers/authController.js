4af8c65bc9bad54623d1c02220595dd539a05166
anuzsubedi <anuzsubedi@hotmail.com>
2025-01-10 00:04:54 -0500

    Add uuid validation to authentication request

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,13 +2,13 @@ import { handleAuth } from "../services/authService.js";
 
 export const auth = (req, res, next) => {
     try {
-        const { email, name, image } = req.body;
+        const { email, name, image, uuid } = req.body;
 
-        if (!email || !name) {
-            return res.status(400).json({ error: "Email and name are required." });
+        if (!email || !name || !uuid) {
+            return res.status(400).json({ error: "Email, name, and uuid are required." });
         }
 
-        const user = handleAuth({ email, name, image });
+        const user = handleAuth({ email, name, image, uuid });
         res.status(200).json({ success: true, user });
     } catch (err) {
         next(err);
